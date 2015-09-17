0535ffbaf4177753f9aff61156ccc6239ea6b34b
jzhou15 <jiazhong@iastate.edu>
2015-09-17 16:19:01 -0500

    hw2

---
+++ Homework_2/p1/ME557_HW2_Problem1.cpp
@@ -0,0 +1,28 @@
+// ME557_HW2_Problem1.cpp : Defines the entry point for the console application.
+// modified by Jiazhong Zhou
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+double myFunction(int n)
+{
+	unsigned int i = 0, j = 1, t, k;
+	for (k = 0; k <= n; ++k)
+	{
+		t = i + j;
+		i = j;
+		j = t;
+	}
+	return j;
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+	int p = myFunction(10);
+	cout << p << endl;
+	system("PAUSE");
+}
